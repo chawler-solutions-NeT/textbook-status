3e4a5ffb7f92ddfdcc522b2832ae65016f127d2f
petprog <farinupeter@gmail.com>
2024-05-06 17:37:38 +0100

    add starting backend api folder

---
+++ api/createApp.js
@@ -0,0 +1,36 @@
+import express from "express";
+// import routes from "./routes/index.js";
+import cookieParser from "cookie-parser";
+import path from "path";
+import cors from "cors";
+import { corsOptions } from "./configs/corsOptions.js";
+
+export default function createApp() {
+  const __dirname = path.resolve();
+  const app = express();
+  app.use(cors(corsOptions));
+
+  app.use(express.json());
+
+  app.use(cookieParser());
+
+  // app.use(routes);
+
+  app.use(express.static(path.join(__dirname, "/client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  });
+
+  app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    return res.status(statusCode).send({
+      success: false,
+      statusCode,
+      message,
+    });
+  });
+
+  return app;
+}
